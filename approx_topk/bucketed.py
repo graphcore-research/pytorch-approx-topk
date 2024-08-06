3533eba42f6e65df1a98d877a8faf18d198e8f19
Oscar Key <oscar.t.key@gmail.com>
2024-08-06 14:35:21 +0000

    Add bucket() which constructs an approx top k from an exact one.

---
+++ approx_topk/bucketed.py
@@ -0,0 +1,38 @@
+import math
+
+import torch
+import torch.nn.functional as F
+from torch import Tensor
+
+from approx_topk import TopK
+
+
+def bucket(exact_method: TopK, k_per_bucket: int) -> TopK:
+    """Construct an approximate, parallelised top k by bucketing an exact method."""
+
+    def bucketed(xs: Tensor, k: int, dim: int) -> tuple[Tensor, Tensor]:
+        if k % k_per_bucket != 0:
+            raise NotImplementedError
+        if dim != -1 and dim != xs.ndim - 1:
+            raise NotImplementedError
+
+        n_buckets = k // k_per_bucket
+        base_shape = xs.shape[:-1]
+        topk_size = xs.shape[-1]
+
+        pad_size = n_buckets * int(math.ceil(topk_size / n_buckets)) - topk_size
+        xs = F.pad(xs, pad=(0, pad_size), value=-10000000.0)
+        bucket_size = (topk_size + pad_size) // n_buckets
+        bucketed_xs = xs.reshape(*base_shape, n_buckets, bucket_size)
+
+        values, indices = exact_method(bucketed_xs, k_per_bucket, dim=-1)
+
+        indices_correction = (
+            torch.arange(0, topk_size, bucket_size, device=indices.device)
+            .unsqueeze(-1)
+            .expand_as(indices)
+        )
+        corrected_indices = indices + indices_correction
+        return values.reshape(*base_shape, k), corrected_indices.reshape(*base_shape, k)
+
+    return bucketed
---
+++ tests/test_bucketed.py
@@ -0,0 +1,55 @@
+import pytest
+import torch
+
+from approx_topk.bucketed import bucket
+from approx_topk.exact_methods import torch_default
+
+
+def test__bucket__k_not_divisible_by_k_per_bucket__raises() -> None:
+    bucketed_topk = bucket(torch_default.topk, k_per_bucket=12)
+    xs = torch.randn(1024)
+    with pytest.raises(NotImplementedError):
+        bucketed_topk(xs, 13, dim=0)
+
+
+def test__bucket__top_k_values_ideally_distributed__equal_to_exact_top_k() -> None:
+    bucketed_topk = bucket(torch_default.topk, k_per_bucket=2)
+    k = 8
+    # Make sure input size is not divisible by number of buckets (in this case 4) to
+    # test padding code.
+    xs = torch.full((2, 1025), fill_value=0.0)
+    # Ensure each bucket contains two of the top k.
+    xs[0, 1] = 10.0
+    xs[0, 2] = 11.0
+    xs[0, 300] = 11.0
+    xs[0, 301] = 12.0
+    xs[0, 600] = 11.0
+    xs[0, 601] = 12.0
+    xs[0, 900] = 13.0
+    xs[0, 901] = 12.0
+    xs[1, 5] = 10.0
+    xs[1, 6] = 11.0
+    xs[1, 302] = 11.0
+    xs[1, 305] = 12.0
+    xs[1, 610] = 11.0
+    xs[1, 611] = 12.0
+    xs[1, 920] = 13.0
+    xs[1, 921] = 12.0
+
+    values, indices = bucketed_topk(xs, k, dim=1)
+    expected_values, expected_indices = torch_default.topk(xs, k, dim=1)
+
+    assert torch.allclose(values.sort().values, expected_values.sort().values)
+    assert torch.allclose(indices.sort().values, expected_indices.sort().values)
+
+
+def test__bucket__only_one_bucket__equal_to_exact_top_k() -> None:
+    k = 128
+    bucketed_topk = bucket(torch_default.topk, k_per_bucket=k)
+    xs = torch.randn(1024)
+
+    values, indices = bucketed_topk(xs, k, dim=0)
+    expected_values, expected_indices = torch_default.topk(xs, k, dim=0)
+
+    assert torch.allclose(values.sort().values, expected_values.sort().values)
+    assert torch.allclose(indices.sort().values, expected_indices.sort().values)
