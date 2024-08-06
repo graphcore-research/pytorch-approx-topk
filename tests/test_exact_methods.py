243cbdb6ed37f922b5c80a370b376bd590554d02
Oscar Key <oscar.t.key@gmail.com>
2024-08-06 10:40:09 +0000

    Add some more tests.

--- tests/test_exact_methods.py
+++ tests/test_exact_methods.py
@@ -13,6 +13,17 @@ def test__radix_select__with_batch__equal_to_built_in() -> None:
     _test(radix_select.topk, xs_shape=(256, 512, 128), dim=2)
 
 
+def test__radix_select__call_twice_on_same_data__does_not_crash() -> None:
+    xs = torch.randn((128,), device="cuda:0")
+    radix_select.topk(xs, k=128, dim=0)
+    radix_select.topk(xs, k=128, dim=0)
+
+
+def test__radix_select__negative_dim__does_not_crash() -> None:
+    xs = torch.randn((128, 256), device="cuda:0")
+    radix_select.topk(xs, k=128, dim=-1)
+
+
 def _test(method: ExactTopK, xs_shape: tuple[int, ...], dim: int) -> None:
     for k in [0, 12, 50, 100, xs_shape[dim]]:
         xs = torch.randn(xs_shape, device="cuda:0")
