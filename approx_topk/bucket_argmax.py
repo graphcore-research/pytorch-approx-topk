9756afa2be10a9202772299080fa0dca20e03559
Douglas Orr <douglaso@graphcore.ai>
2024-08-09 15:17:57 +0000

    Improve benchmark timing and add new bucket-argmax implementations

--- approx_topk/bucket_argmax.py
+++ approx_topk/bucket_argmax.py
@@ -1,14 +1,93 @@
 """A technique with k=1 for each bucket."""
 
 import torch
+import triton
+import triton.language as tl
 from torch import Tensor
+from triton import cdiv
 
 from . import autobucket
 
-def _argmax_topk(xs: Tensor, k: int, dim: int):
+
+def _argmax_topk(xs: Tensor, k: int, dim: int) -> tuple[Tensor, Tensor]:
     assert k == 1
     return torch.max(xs, dim=dim, keepdim=True)
 
-topk = autobucket.bucket(_argmax_topk, k_per_bucket=1)
-topk.__name__ = "topk"
-topk.__module__ = _argmax_topk.__module__
+
+topk_autobucket = autobucket.bucket(_argmax_topk, k_per_bucket=1)
+topk_autobucket.__name__ = "topk_autobucket"
+topk_autobucket.__module__ = _argmax_topk.__module__
+
+
+def topk_torch(xs: Tensor, k: int, dim: int) -> tuple[Tensor, Tensor]:
+    dim = dim % xs.ndim  # convert negative dims to positive
+    xs_bucketed = torch.nn.functional.pad(
+        xs,
+        pad=(0, 0) * len(xs.shape[dim + 1 :]) + (0, -xs.shape[dim] % k),
+        value=torch.finfo(xs.dtype).min,
+    ).unflatten(dim, (k, -1))
+    max_ = xs_bucketed.max(dim=dim + 1)
+    indices = max_.indices.add_(
+        xs_bucketed.shape[dim + 1]
+        * torch.arange(0, k, dtype=max_.indices.dtype, device=xs.device)
+    )
+    return (max_.values, indices)
+
+
+@triton.jit
+def _topk_triton_kernel(
+    xs_ptr,
+    values_out_ptr,
+    indices_out_ptr,
+    b: int,
+    k: int,
+    n: int,
+    n_chunk: int,
+    xs_stride: int,
+    BLOCK_SIZE: tl.constexpr,
+):
+    pidx = tl.program_id(axis=0).to(tl.int64)
+    bk_idx = BLOCK_SIZE * pidx + tl.arange(0, BLOCK_SIZE)
+    b_idx, k_idx = bk_idx // k, bk_idx % k
+    xs_ptr += b_idx * xs_stride + k_idx * n_chunk
+
+    max_value = tl.load(xs_ptr, mask=(b_idx < b))
+    max_index = tl.zeros((BLOCK_SIZE,), tl.int64)
+    for i in tl.range(1, n_chunk):
+        mask = (b_idx < b) & (k_idx * n_chunk + i < n)
+        block = tl.load(xs_ptr + i, mask=mask)
+        mask &= max_value < block
+        max_value = tl.where(mask, block, max_value)
+        max_index = tl.where(mask, i, max_index)
+
+    max_index += k_idx * n_chunk
+    tl.store(values_out_ptr + b_idx * k + k_idx, max_value, mask=(b_idx < b))
+    tl.store(indices_out_ptr + b_idx * k + k_idx, max_index, mask=(b_idx < b))
+
+
+def topk_triton(xs: Tensor, k: int, dim: int, block_size: int) -> tuple[Tensor, Tensor]:
+    dim = dim % xs.ndim  # convert negative dims to positive
+    if dim != xs.ndim - 1:
+        values, indices = topk_triton(
+            xs.movedim(dim, -1), k=k, dim=-1, block_size=block_size
+        )
+        return values.movedim(-1, dim), indices.movedim(-1, dim)
+
+    xs = xs.contiguous()
+    values = torch.empty(xs.shape[:-1] + (k,), device="cuda", dtype=xs.dtype)
+    indices = torch.empty(values.shape, device="cuda", dtype=torch.int64)
+
+    n = xs.shape[-1]
+    b = xs.nelement() // n
+    _topk_triton_kernel[(cdiv(b * k, block_size),)](
+        xs_ptr=xs,
+        values_out_ptr=values,
+        indices_out_ptr=indices,
+        b=b,
+        k=k,
+        n=n,
+        n_chunk=cdiv(n, k),
+        xs_stride=xs.stride(-2),
+        BLOCK_SIZE=block_size,
+    )
+    return values, indices
--- benchmarks/measure_speed.py
+++ benchmarks/measure_speed.py
@@ -7,6 +7,7 @@ from typing import Any, Optional
 
 import torch
 import tqdm
+from torch import Tensor
 from torch.cuda import Event
 
 from approx_topk import TopK, bucket_argmax
@@ -22,9 +23,9 @@ class Experiment:
     topk_size: int
     k: int
     dtype: torch.dtype
-    n_outer: int = 50
-    n_inner: int = 20
-    n_warmup: int = 200
+    n_warmup: int = 16
+    n_outer: int = 16
+    n_inner: int = 128
 
     def save(self) -> dict[str, Any]:
         d = self.__dict__.copy()
@@ -35,26 +36,48 @@ class Experiment:
         return d
 
 
+def fake_topk_sum(xs: Tensor, k: int, dim: int) -> tuple[Tensor, Tensor]:
+    y = xs.sum(dim=dim, keepdim=True)  # ignore k, just run a sum
+    return y, torch.zeros(y.shape, dtype=torch.long, device=y.device)
+
 
 def run(config: Experiment) -> list[float]:
     durations: list[float] = []
 
-    method = lambda xs: config.method(xs, k=config.k, dim=1, **config.args)
+    def _inner_loop_fn(xs: Tensor) -> list[Tensor]:
+        return [
+            config.method(xs[i], k=config.k, dim=1, **config.args)[0]
+            for i in range(config.n_inner)
+        ]
+
     if config.compile:
         torch.compiler.reset()
-        method = torch.compile(method, mode=config.compile)
+        _inner_loop_fn = torch.compile(
+            _inner_loop_fn, mode=config.compile, fullgraph=True, dynamic=False
+        )
 
-    xs = torch.randn(config.batch_size, config.topk_size, dtype=config.dtype, device="cuda")
+    xs = torch.randn(
+        config.n_inner,
+        config.batch_size,
+        config.topk_size,
+        dtype=config.dtype,
+        device="cuda",
+    )
     for _ in range(config.n_warmup):
-        method(xs)
+        _inner_loop_fn(xs)
 
     for _ in range(config.n_outer):
-        xs = torch.randn(config.batch_size, config.topk_size, dtype=config.dtype, device="cuda")
+        xs = torch.randn(
+            config.n_inner,
+            config.batch_size,
+            config.topk_size,
+            dtype=config.dtype,
+            device="cuda",
+        )
         start_event = Event(enable_timing=True)
         end_event = Event(enable_timing=True)
         start_event.record()
-        for _ in range(config.n_inner):
-            method(xs)
+        _inner_loop_fn(xs)
         end_event.record()
         end_event.synchronize()
         durations.append(start_event.elapsed_time(end_event) / 1e3)
@@ -66,21 +89,46 @@ def sweep(configs: list[Experiment], out: Path) -> None:
     out.parent.mkdir(exist_ok=True)
     with out.open("w") as f:
         for config in tqdm.tqdm(configs):
-            print(json.dumps(dict(**config.save(), duration=run(config))), file=f, flush=True)
+            print(
+                json.dumps(
+                    dict(
+                        **config.save(),
+                        duration=run(config),
+                        device=torch.cuda.get_device_name(),
+                    )
+                ),
+                file=f,
+                flush=True,
+            )
 
 
 if __name__ == "__main__":
-    sweep([Experiment(
-            method=method,
-            args={},
-            compile=compile,
-            batch_size=32,
-            topk_size=topk_size,
-            k=topk_size//8,
-            dtype=torch.float32,
-        )
-        for compile in [None, "default", "reduce-overhead", "max-autotune"]
-        for method in [radix_select.topk, torch_default.topk, bucket_argmax.topk]
-        # for topk_size in [2**n for n in range(10, 16+1, 1)]
-        for topk_size in range(1024, 16384 + 1, 1024)
-    ], Path("results/measure_speed.jsonl"))
+    sweep(
+        [
+            Experiment(
+                method=method,
+                args={},
+                compile=compile,
+                batch_size=32,
+                topk_size=topk_size,
+                k=topk_size // 8,
+                dtype=torch.float32,
+            )
+            for compile in [
+                None,
+                "default",
+                "reduce-overhead",
+                # "max-autotune",
+            ]
+            for method in [
+                fake_topk_sum,
+                torch_default.topk,
+                bucket_argmax.topk_autobucket,
+                bucket_argmax.topk_torch,
+                # radix_select.topk,
+            ]
+            for topk_size in [2**n for n in [12, 14, 16]]
+            if not (compile and method is radix_select.topk)
+        ],
+        Path("results/measure_speed.jsonl"),
+    )
