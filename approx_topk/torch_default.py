0281157073e6f951a409e536e7823cd96e5eb1c6
Luka Ribar <lukar@graphcore.ai>
2024-09-05 14:08:07 +0000

    Add reference bucketed torch top-k

--- approx_topk/torch_default.py
+++ approx_topk/torch_default.py
@@ -1,8 +1,28 @@
-"""Wrapper around torch.topk() to match our topk Protocol."""
+"""Wrapper around torch.topk() to match our topk Protocol,
+as well as the reference bucketed top-k implementation using torch.topk
+"""
 
 import torch
 from torch import Tensor
 
+from approx_topk.autobucket import bucket
+
 
 def topk(xs: Tensor, k: int, dim: int) -> tuple[Tensor, Tensor]:
     return torch.topk(xs, k, dim, sorted=False)
+
+
+def bucket_topk(
+    xs: Tensor,
+    k: int,
+    dim: int,
+    l_multiplier: int,
+    k_per_bucket: int,
+    interleaved: bool,
+) -> tuple[Tensor, Tensor]:
+    return bucket(
+        topk,
+        l_multiplier=l_multiplier,
+        k_per_bucket=k_per_bucket,
+        interleaved=interleaved,
+    )(xs, k, dim)
